1066175c2856dd3f95d59434dfe8f87e0f19861a
Bogdan Stroe <bogdan.stroe@gmail.com>
2022-11-28 10:18:37 +0200

    strings and fractions

---
+++ substring_count.py
@@ -0,0 +1,14 @@
+def count_substring(string, sub_string):
+    counter = 0
+    index = string.find(sub_string)
+    while index != -1:
+        counter += 1
+        index = string.find(sub_string, index + 1)
+    return counter
+
+if __name__ == '__main__':
+    string = input().strip()
+    sub_string = input().strip()
+    
+    count = count_substring(string, sub_string)
+    print(count)
